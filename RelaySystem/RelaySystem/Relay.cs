ee250a5019795e9bdf4046805515d2744fae78ff
Jarosław Krefta <jaroslaw.krefta@jetshop.se>
2018-06-20 12:37:51 +0200

    Started implementation of Relay.RelayMessage [wip]

--- RelaySystem/RelaySystem/Relay.cs
+++ RelaySystem/RelaySystem/Relay.cs
@@ -1,6 +1,23 @@
-﻿namespace RelaySystem
+﻿using System;
+using System.Threading.Tasks;
+using RelaySystem.Models;
+
+namespace RelaySystem
 {
     public class Relay
     {
+        public Task<bool> RelayMessage(Message message)
+        {
+            ValidateThatMessageIsNotNull(message);
+            return Task.FromResult(true);
+        }
+
+        private static void ValidateThatMessageIsNotNull(Message message)
+        {
+            if (message == null)
+            {
+                throw new ArgumentNullException(nameof(message));
+            }
+        }
     }
 }
