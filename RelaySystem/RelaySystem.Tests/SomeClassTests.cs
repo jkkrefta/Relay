b6efa7fbdfbbeecfd97e9ca5c7ec1f0d2e1cdae2
Jarosław Krefta <jaroslaw.krefta@jetshop.se>
2018-06-20 11:58:36 +0200

    Initial App Setup

---
+++ RelaySystem/RelaySystem.Tests/SomeClassTests.cs
@@ -0,0 +1,14 @@
+﻿namespace RelaySystem.Tests
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class SomeClassTests
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.That(true, Is.True);
+        }
+    }
+}
---
+++ RelaySystem/RelaySystem/Relay.cs
@@ -0,0 +1,6 @@
+﻿namespace RelaySystem
+{
+    public class Relay
+    {
+    }
+}
